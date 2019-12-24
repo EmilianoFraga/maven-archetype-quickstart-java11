ab4909886e99ab50f42514f3b9d7c0e2a5aa7c76
Emiliano Vaz Fraga <granamirdev@gmail.com>
2019-12-24 11:12:54 +0100

    Remove final from main method

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,7 +1,7 @@
 package ${groupId};
 
 public class App {
-    public static final void main (String... args) {
+    public static void main (String... args) {
         System.out.println("App is running");
     }
 }
