f29e5ee297b949be9c4c5d7f74222650169945d6
Emiliano Vaz Fraga <efraga@gmail.com>
2020-03-06 10:23:57 +0100

    [TASK] Proper Unit Test comments

--- src/main/resources/archetype-resources/src/test/java/AppTest.java
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -3,9 +3,24 @@ package ${groupId};
 import org.junit.*;
 
 public class AppTest {
+	@Before
+	public void setup() {
+		// This is an empty, placeholder method
+	}
+
+
 	@Test
 	public void testApp() {
-		Assert.assertTrue("Expected result should be true", 1 == 1);
+		// This is a placeholder
+
+		// GIVEN
+
+		// WHEN
+		int expected = 1;
+		int actual = 1;
+
+		// THEN
+		Assert.assertEquals("Expected result should be true", expected, actual);
 	}
 }
 
